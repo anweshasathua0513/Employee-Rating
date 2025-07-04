8b70d6f37e04380c5c77ef2399368230309a697d
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-07-04 11:29:16 +0530

    Add files via upload

---
+++ Employee-main/.angular/cache/17.3.17/17.3.17/vite/deps/browser-D5GTT4TU.js
@@ -0,0 +1,50 @@
+import {
+  Animation,
+  AnimationDriver,
+  AnimationEngine,
+  AnimationRenderer,
+  AnimationRendererFactory,
+  AnimationStyleNormalizer,
+  BaseAnimationRenderer,
+  NoopAnimationDriver,
+  NoopAnimationStyleNormalizer,
+  WebAnimationsDriver,
+  WebAnimationsPlayer,
+  WebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge,
+  camelCaseToDashCase,
+  containsElement,
+  createEngine,
+  getParentElement,
+  invokeQuery,
+  normalizeKeyframes,
+  validateStyleProperty,
+  validateWebAnimatableStyleProperty
+} from "./chunk-NBVC6V2Z.js";
+import "./chunk-5DHWLQGT.js";
+import "./chunk-O65QOLNO.js";
+import "./chunk-2SBDAC72.js";
+export {
+  AnimationDriver,
+  NoopAnimationDriver,
+  Animation as ɵAnimation,
+  AnimationEngine as ɵAnimationEngine,
+  AnimationRenderer as ɵAnimationRenderer,
+  AnimationRendererFactory as ɵAnimationRendererFactory,
+  AnimationStyleNormalizer as ɵAnimationStyleNormalizer,
+  BaseAnimationRenderer as ɵBaseAnimationRenderer,
+  NoopAnimationStyleNormalizer as ɵNoopAnimationStyleNormalizer,
+  WebAnimationsDriver as ɵWebAnimationsDriver,
+  WebAnimationsPlayer as ɵWebAnimationsPlayer,
+  WebAnimationsStyleNormalizer as ɵWebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge as ɵallowPreviousPlayerStylesMerge,
+  camelCaseToDashCase as ɵcamelCaseToDashCase,
+  containsElement as ɵcontainsElement,
+  createEngine as ɵcreateEngine,
+  getParentElement as ɵgetParentElement,
+  invokeQuery as ɵinvokeQuery,
+  normalizeKeyframes as ɵnormalizeKeyframes,
+  validateStyleProperty as ɵvalidateStyleProperty,
+  validateWebAnimatableStyleProperty as ɵvalidateWebAnimatableStyleProperty
+};
+//# sourceMappingURL=browser-D5GTT4TU.js.map
