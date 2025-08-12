5c868c735c69f3549c1f6374381405b16dbdfcf9
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-08-12 15:54:59 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 import { EmployeeComponent } from './app/employee/employee.component';
 import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-bootstrapApplication(EmployeeComponent)
-  .catch(err => console.error(err));
 bootstrapApplication(EmployeeComponent, {
   providers: [
-    provideHttpClient()
+    provideHttpClient() // Enables HttpClient throughout your app
   ]
-});
+})
+.catch(err => console.error(err));
